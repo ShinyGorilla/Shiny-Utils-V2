9e8a40d98f4d2edbc0ec19905f8547ee820cfd83
ShinyGorilla <134111158+ShinyGorilla@users.noreply.github.com>
2024-02-20 15:17:52 -0700

    Update GNotRPCPatch.cs

--- Patches/GNotPatching/GNotRPCPatch.cs
+++ Patches/GNotPatching/GNotRPCPatch.cs
@@ -1,21 +1,20 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using MonkeNotificationLib;
 using UnityEngine;
-using Photon.Pun;
 using Shiny_Utils_V2.UI;
 
 namespace Shiny_Utils_V2.Patches.GNotPatching
 {
-    [HarmonyPatch(typeof(GorillaNot), "IncrementRPCCallLocal")]
-    internal class GNotRPCPatch
+    [HarmonyPatch(typeof(GorillaNot), "SendReport")]
+    internal class GNotReportPatch
     {
-        static void Postfix(PhotonMessageInfo info, string rpcFunction)
+        static void Postfix(string susReason, string susId, string susNick)
         {
-            if (ShinyGui.GnotNotif && !rpcFunction.Contains("PlayerHandTap"))
+            if (ShinyGui.GnotNotif)
             {
-                NotificationController.AppendMessage("RPC".WrapColor("warning"), $"[{rpcFunction.ToUpper().WrapColor("cyan")}] Called by: {info.Sender.NickName.ToUpper().WrapColor("cyan")}");
+                NotificationController.AppendMessage("Gorilla Not".WrapColor("danger"), $"Anticheat reported {susNick.ToUpper().WrapColor("cyan")} for \"{susReason.WrapColor("danger")}\" - NOT ALL REPORTS ARE ACCURATE!");
             }
-            Debug.Log($"{rpcFunction} called by {info.Sender.NickName} (ID: {info.Sender.UserId})");
+            Debug.LogError($"GNot reported {susNick} for {susReason} (ID: {susId})");
         }
     }
 }
